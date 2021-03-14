a536256d226e9af8620bdc70ad73380f726b5661
Olga Shitova <olgashitova@MacBook-Pro-2.local>
2021-03-14 15:30:03 +0200

    fix styling

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -2,10 +2,40 @@ import React from 'react';
 import styled from 'styled-components';
 import FrontImg from '../../Assets/meow.jpg'
 
+const Card = ({img, alt, cat, onCardClick, id, show}) => {
+    console.log('img', img);
+    return (
+        <CardItem onClick={() => onCardClick(alt, id)}>
+            <CardInner show={show}>
+                <FrontSide>
+                    <Img src={FrontImg} alt='meow'/>
+                </FrontSide>
+                <BackSide background={img} cat={cat}>
+                    <Img src={img} alt={alt} cat={cat}/>
+                </BackSide>
+            </CardInner>
+        </CardItem>
+    );
+};
+
+function areEqual(prevProps, nextProps) {
+    if (prevProps.show === nextProps.show) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export default React.memo(Card, areEqual);
+
+
 const CardInner = styled.div`
   position: relative;
+  margin: 10px;
   width: 100%;
-  height: 100%;
+  height: auto;
+  max-width: 140px;
+  max-height: 140px;
   text-align: center;
   transition: transform 0.6s;
   transform-style: preserve-3d;
@@ -15,14 +45,11 @@ const CardInner = styled.div`
 `
 
 const CardItem = styled.li`
-  padding: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
   max-width: 140px;
   max-height: 140px;
-  min-width: 140px;
-  min-height: 140px;
   background: #fff;
   position: relative;
   perspective: 1000px;
@@ -40,43 +67,21 @@ const FrontSide = styled.div`
   display: flex;
   justify-content: center;
   border-radius: 10%;
+  max-height: 100%;
 `
 const BackSide = styled.div`
   transform: rotateY(180deg);
-  height: 100%;
+  height: auto;
   width: 100%;
   background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const Img = styled.img`
   display: block;
   max-width: 100%;
-  height: auto;
   max-height: 100%;
+  height: auto;
 `;
-
-const Card = ({img, alt, cat, onCardClick, id, show}) => {
-
-    return (
-        <CardItem onClick={() => onCardClick(alt, id)}>
-            <CardInner show={show}>
-                <FrontSide>
-                    <Img src={FrontImg} alt='meow'/>
-                </FrontSide>
-                <BackSide>
-                    <Img src={img} alt={alt} cat={cat}/>
-                </BackSide>
-            </CardInner>
-        </CardItem>
-    );
-};
-
-function areEqual(prevProps, nextProps) {
-    if (prevProps.show === nextProps.show) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-export default React.memo(Card, areEqual);
